b00bb8b141b2995b5c5b061dc0ec57e77affdf7e
riyaz <riyazpasha880@gmail.com>
2021-03-02 23:31:53 +0530

    created post create component with minimal bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import PostCreate from './PostCreate'
 
 const App = () => {
   return (
-    <div>
-      Blog App
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
     </div>
   )
 }
