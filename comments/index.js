813dbf464ac71d291577bc3183ee8b201c4e1abd
riyaz <riyazpasha880@gmail.com>
2021-03-03 23:08:24 +0530

    emit comment created event to event bus whenever new comment is created for a post

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,7 +14,7 @@ app.get("/posts/:id/comments", (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
     const commentId = randomBytes(4).toString("hex");
     const { content } = req.body;
 
@@ -21,6 +22,15 @@ app.post("/posts/:id/comments", (req, res) => {
     comments.push({ commentId, content });
 
     commentsByPostId[req.params.id] = comments;
+
+    await axios.post("http://localhost:4005/events", {
+        title: "CommentCreated",
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id
+        }
+    })
 
     res.status(201).send(comments);
 });
