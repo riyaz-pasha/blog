423d87c2aa280018279c058d2bc9a9b1e1d35348
riyaz <riyazpasha880@gmail.com>
2021-03-04 23:15:44 +0530

    display appropriate message on website according to the comment status

--- client/src/CommentList.jsx
+++ client/src/CommentList.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 
 const CommentList = ({ comments }) => {
     const renderComments = comments.map(comment => {
-        return <li key={comment.id}>{comment.content}</li>
+        let content;
+        if (comment.status === "approved") {
+            content = comment.content;
+        }
+
+        if (comment.status === "rejeccted") {
+            content = "This comment has been rejected";
+        }
+
+        if (comment.status === "pending") {
+            content = "This comment is awaiting moderation";
+        }
+        return <li key={comment.id}>{content}</li>
     })
 
     return (
