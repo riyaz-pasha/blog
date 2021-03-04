26a14959d1979b7f1ca72b6f1d88ec0a6587a96b
riyaz <riyazpasha880@gmail.com>
2021-03-04 22:39:13 +0530

    comment service will listen for comment moderated event from eventbus and update comment status in  local data base and punlish one more event called comment updated to the event bus

--- comments/index.js
+++ comments/index.js
@@ -36,8 +36,27 @@ app.post("/posts/:id/comments", async (req, res) => {
     res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
-    console.log("Event Recieved : ", req.body.type);
+app.post("/events", async (req, res) => {
+    const { type, data } = req.body;
+    console.log("Event Recieved : ", type);
+
+    if (type === "CommentModerated") {
+        const { postId, id, content, status } = data;
+        const comments = commentsByPostId[postId];
+
+        const comment = comments.find(comment => comment.id === id);
+        comment.status = status;
+
+        await axios.post("http://loccalhost:4005/events", {
+            type: "CommentUpdated",
+            data: {
+                postId,
+                id,
+                content,
+                status
+            }
+        });
+    }
 
     res.send({});
 })
